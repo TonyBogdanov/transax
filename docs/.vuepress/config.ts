40fd62121d176f2e28f1421fb75f8a2674111604
Tony Bogdanov <tonybogdanov@gmail.com>
2023-08-19 13:58:51 +0300

    feat: docs search & custom font

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,5 +1,6 @@
 import { defineUserConfig } from 'vuepress';
 import { hopeTheme } from 'vuepress-theme-hope';
+import { searchProPlugin } from 'vuepress-plugin-search-pro';
 
 export default defineUserConfig( {
     base: '/transax/',
@@ -8,9 +9,16 @@ export default defineUserConfig( {
     description: 'Framework-agnostic JavaScript library for content internationalization.',
 
     plugins: [
+        searchProPlugin( {
+            indexContent: true,
+        } ),
     ],
 
     head: [
+        [ 'link', { rel: 'preconnect', href: 'https://fonts.googleapis.com' } ],
+        [ 'link', { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' } ],
+        [ 'link', { href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap', rel: 'stylesheet' } ],
+
         [ 'link', { rel: 'icon', href: '/assets/favicon.ico' } ],
         [ 'link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/assets/apple-touch-icon.png' } ],
         [ 'link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/assets/favicon-32x32.png' } ],
@@ -34,35 +42,39 @@ export default defineUserConfig( {
         sidebar: [
             { text: 'Introduction', link: '/introduction' },
             { text: 'Installation', link: '/installation' },
-            { text: 'Usage', link: '/usage/importing', collapsible: true, children: [
-                '/usage/importing',
-                '/usage/analysis',
-                '/usage/compilation',
-                '/usage/translation',
-                '/usage/interpolation',
-                '/usage/syntax',
-            ] },
+            {
+                text: 'Usage', link: '/usage/importing', collapsible: true, children: [
+                    '/usage/importing',
+                    '/usage/analysis',
+                    '/usage/compilation',
+                    '/usage/translation',
+                    '/usage/interpolation',
+                    '/usage/syntax',
+                ]
+            },
             { text: 'Advanced', link: '/advanced' },
-            { text: 'API Reference', link: '/api', collapsible: true, children: [
-                '/api/interfaces/LoggerInterface',
-                '/api/interfaces/AnalyzerInterface',
-                '/api/interfaces/CompilerInterface',
-                '/api/interfaces/GeneratorInterface',
-                '/api/interfaces/TranslatorInterface',
-                '/api/classes/Logger',
-                '/api/classes/Analyzer',
-                '/api/classes/AnalyzerToken',
-                '/api/classes/Compiler',
-                '/api/classes/CompilerContext',
-                '/api/classes/CompilerToken',
-                '/api/classes/TextToken',
-                '/api/classes/LiteralToken',
-                '/api/classes/TernaryExpressionToken',
-                '/api/classes/ComparisonExpressionToken',
-                '/api/classes/CallExpressionToken',
-                '/api/classes/Generator',
-                '/api/classes/Translator',
-            ] },
+            {
+                text: 'API Reference', link: '/api', collapsible: true, children: [
+                    '/api/interfaces/LoggerInterface',
+                    '/api/interfaces/AnalyzerInterface',
+                    '/api/interfaces/CompilerInterface',
+                    '/api/interfaces/GeneratorInterface',
+                    '/api/interfaces/TranslatorInterface',
+                    '/api/classes/Logger',
+                    '/api/classes/Analyzer',
+                    '/api/classes/AnalyzerToken',
+                    '/api/classes/Compiler',
+                    '/api/classes/CompilerContext',
+                    '/api/classes/CompilerToken',
+                    '/api/classes/TextToken',
+                    '/api/classes/LiteralToken',
+                    '/api/classes/TernaryExpressionToken',
+                    '/api/classes/ComparisonExpressionToken',
+                    '/api/classes/CallExpressionToken',
+                    '/api/classes/Generator',
+                    '/api/classes/Translator',
+                ]
+            },
         ],
     } ),
 } );
