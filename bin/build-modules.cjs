b4fd7d702bc244b55247f0837826d0f61df64609
Tony Bogdanov <tonybogdanov@gmail.com>
2023-08-24 16:47:57 +0300

    feat: use explicit extensions for cjs & esm

--- bin/build-modules.cjs
+++ bin/build-modules.cjs
@@ -6,10 +6,10 @@
 const glob = require( 'glob' );
 const uglify = require( 'uglify-js' );
 
-const { readFile, writeFile } = require( 'node:fs/promises' );
+const { readFile, writeFile, rename } = require( 'node:fs/promises' );
 const { resolve } = require( 'node:path' );
 const { rimraf } = require( 'rimraf' );
-const { relative } = require( 'path' );
+const { relative, extname } = require( 'path' );
 const { spawn } = require( 'child_process' );
 
 const execute = ( command, args, env = {} ) => new Promise(
@@ -17,7 +17,7 @@ const execute = ( command, args, env = {} ) => new Promise(
         .on( 'close', code => 0 === code ? resolve() : reject( code ) )
 );
 
-const run = async ( target, type ) => {
+const run = async ( target, type, ext ) => {
     await rimraf( resolve( __dirname, '..', target ), { preserveRoot: true } );
     await execute( 'tsc', [ '--build', resolve( __dirname, `../${ target }.tsconfig.json` ) ] );
 
@@ -38,12 +38,18 @@ const run = async ( target, type ) => {
 
     await writeFile( resolve( __dirname, '..', target, 'package.json' ), JSON.stringify( cfg, null, 2 ) );
 
-    await Promise.all( glob.sync( resolve( __dirname, '..', target, '**/*.js' ) ).map( async path => {
+    await Promise.all( glob.sync( resolve( __dirname, '..', target, '**/*.js' ) )
+        .map( path => rename( path, path.substring( 0, path.length - extname( path ).length ) + ext ) ) );
+
+    await Promise.all( glob.sync( resolve( __dirname, '..', target, '**/*' + ext ) ).map( async path => {
         const min = uglify.minify( await readFile( path, 'utf8' ), { sourceMap: {} } );
         return Promise.all( [ writeFile( path, min.code ), writeFile( path + '.map', min.map ) ] );
     } ) );
 };
 
 ( async () => {
-    await Promise.all( [ run( 'cjs', 'commonjs' ), run( 'esm', 'module' ) ] );
+    await Promise.all( [
+        run( 'cjs', 'commonjs', '.cjs' ),
+        run( 'esm', 'module', '.mjs' ),
+    ] );
 } )();
