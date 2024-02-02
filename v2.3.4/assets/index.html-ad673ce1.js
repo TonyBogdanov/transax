bdfe70a7a484476522ce3ae0a253e183e49290ef
TonyBogdanov <TonyBogdanov@users.noreply.github.com>
2024-02-02 07:16:01 +0000

    deploy: 50f504e84070e93da3e56be999ed4df61636309a

---
+++ v2.3.4/assets/index.html-ad673ce1.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-69092c87","path":"/api/interfaces/","title":"Interfaces","lang":"en-US","frontmatter":{"title":"Interfaces","article":false,"feed":false,"sitemap":false},"headers":[],"git":{},"readingTime":{"minutes":0,"words":1},"filePathRelative":null,"excerpt":""}');export{e as data};
